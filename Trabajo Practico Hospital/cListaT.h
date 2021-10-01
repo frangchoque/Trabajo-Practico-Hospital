c37859f190c919e88d035110a04bb73b49df1926
Franco Gabriel Choque <gabriel.choque6561@gmail.com>
2021-10-01 13:22:00 -0300

    Agrego lista template.

---
+++ Trabajo Practico Hospital/cListaT.h
@@ -0,0 +1,171 @@
+#pragma once
+#define N 50
+#include <iostream>
+
+using namespace std;
+
+template <class T>
+class ListaT
+{
+	T** lista;
+	unsigned int CA;
+	unsigned int TAM;
+	void Redimensionalizar();
+
+public:
+	ListaT(unsigned int TAM = N);
+	~ListaT();
+
+	void Agregar(T* nuevo);
+	void Eliminar(unsigned int ID);
+	T* Quitar(unsigned int ID);
+	T* Buscar_por_ID(unsigned int ID);
+	T* Buscar_por_pos(unsigned int pos);
+	unsigned int getCA();
+	int getPos(unsigned int ID);
+
+	T* operator[](unsigned int pos);
+};
+
+template<class T>
+inline void ListaT<T>::Redimensionalizar()
+{
+	TAM = TAM * 2;
+	T** aux = new T * [TAM];
+	for (int i = 0; i < TAM; i++)
+	{
+		if (lista[i] != NULL)
+		{
+			aux[i] = lista[i]
+		}
+		else
+			aux[i] = NULL;
+	}
+	delete[] lista;
+	lista = aux;
+}
+
+template<class T>
+inline ListaT<T>::ListaT(unsigned int TAM)
+{
+	this->TAM = TAM;
+	CA = 0;
+	lista = new T * [TAM];
+	for (int i = 0; i < TAM; i++)
+	{
+		lista[i] = NULL;
+	}
+}
+
+template<class T>
+inline ListaT<T>::~ListaT()
+{
+	if (lista != NULL)
+	{
+		for (int i = 0; i < CA; i++)
+		{
+			if (lista[i] != NULL)
+				delete lista[i];
+		}
+		delete[] lista;
+	}
+}
+
+template<class T>
+inline void ListaT<T>::Agregar(T* nuevo)
+{
+	if (nuevo == NULL)
+		throw new exception("ERROR: Puntero NULL");
+	if (Buscar_por_ID(nuevo->getID()) != NULL)//Controlo que el elemento no este en la lista
+		throw new exception("Ya existe en la lista");//Que todos los que tengan lista tengan un metodo getID()
+	if (this->CA >= TAM)//Si supero la capacidad
+		Redimensionalizar();
+	//Si llega hasta este punto es porque no esta en la lista y nuevo es distinto de NULL
+	lista[CA] = nuevo;
+	CA++;
+}
+
+template<class T>
+inline void ListaT<T>::Eliminar(unsigned int ID)
+{
+	T* aux = NULL;
+	try
+	{
+		aux = Quitar(ID);
+	}
+	catch (exception* error)
+	{
+		throw error;
+	}
+	//Si llego hasta aca no deberia haber problemas
+	delete aux;
+}
+
+template<class T>
+inline T* ListaT<T>::Quitar(unsigned int ID)
+{
+	if (nuevo == NULL)
+		throw new exception("ERROR: Puntero NULL");
+	if (Buscar_por_ID(nuevo->getID()) == NULL)//Controlo que el elemento este en la lista
+		throw new exception("El elemento no existe en la lista");
+	//Si o si van a funcionar
+	aux = Buscar_por_ID(nuevo->getID();
+	int pos = getPos(ID);
+	for (int i = pos; i < CA-1; i++)
+	{
+		lista[i] = lista[i + 1];
+	}
+	CA--;
+
+	return aux;
+}
+
+template<class T>
+inline T* ListaT<T>::Buscar_por_ID(unsigned int ID)
+{
+	T* aux = NULL;
+	for (i = 0; i < CA; i++)
+	{
+		if (lista[i]->getID() == ID)//Si lo encuentro, salgo del for
+		{
+			aux = lista[i];
+			break;
+		}
+	}
+	return aux;
+}
+
+template<class T>
+inline T* ListaT<T>::Buscar_por_pos(unsigned int pos)
+{
+	if (pos >= CA)//Controlo que sea mayor o igual a CA
+		return NULL;
+	return lista[i];
+}
+
+template<class T>
+inline unsigned int ListaT<T>::getCA()
+{
+	return CA;
+}
+
+template<class T>
+inline int ListaT<T>::getPos(unsigned int ID)
+{
+	int pos = -1;
+	for (i = 0; i < CA; i++)
+	{
+		if (lista[i]->getID() == ID)//Si lo encuentro, salgo del for
+		{
+			pos=i;
+			break;
+		}
+	}
+	return pos;
+}
+
+template<class T>
+inline T* ListaT<T>::operator[](unsigned int pos)
+{
+	return Buscar_por_pos(pos);
+}
